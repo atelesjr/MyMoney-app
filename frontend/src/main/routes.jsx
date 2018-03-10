51c4cab79cf96eccb7e2397a04c5842ca8854add
Agostinho F Teles Jr <atelesjr@gmail.com>
2018-03-09 22:03:51 -0300

    Template

---
+++ frontend/src/main/routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+
+import Dashboard from '../dashboard/dashboard'
+import BillingCycle from '../billingCycle/billingCycle'
+
+export default props => (
+    // abaixo admnistar document.history
+    <Router history={hashHistory}>
+        <Route path='/' component={Dashboard} />
+        <Route path='/billingCycles' component={BillingCycle} />
+        <Redirect from='*' to='/' /> 
+        {/* redireciona para o component dashboard.*/}
+    </Router>
+)
