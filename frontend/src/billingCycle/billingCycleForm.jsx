7887e84fd70645ce19d8349eee68b6988e1338ca
Agostinho F Teles Jr <atelesjr@gmail.com>
2018-03-21 20:36:25 -0300

    Messages

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { reduxForm, Field } from 'redux-form'
 
 class BillingCycleForm extends Component {
+    
     render() {
+        const { handleSubmit } = this.props
         return (
-            <form role='form'>
+            <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
+                    <Field name='name' component='input' />
+                    <Field name='month' component='input' />
+                    <Field name='year' component='input' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' 
@@ -15,4 +21,5 @@ class BillingCycleForm extends Component {
     }
 }
 
-export default BillingCycleForm
+//reduxForm tem funcionalidade similar ao connect - decorator
+export default reduxForm({form: 'billingCycleForm'})(BillingCycleForm)
