08075e858bfb1b4fe0b8d68768b6c1b7b5371e23
Agostinho F Teles Jr <atelesjr@gmail.com>
2018-03-22 12:21:16 -0300

    API Restful revisada
    Com comentários revisando  API e  mensagem de erro.

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,32 +1,49 @@
 const BillingCycle = require('./billingCycle')
 const errorHandler = require('../common/errorHandler')
 
+//API Restful
+
+ //método do HTTP
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+// na atualização retorna o novo objeto e não o antigo.
 BillingCycle.updateOptions({new: true, runValidators: true})
 BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
+//Configuração de rotas---------------------------
+//Retorna quantidade de registros da tabela do mongo.
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value)=> {
         if(error){
+            /*dá resposta status 500 e parse para json 
+            com objeto com atributo errors que recebe array de erros */
             res.status(500).json({ errors: [error] })
         } else {
-            res.json({value})
+            //retorna o atributo com a quantidade de registros
+            res.json({value}) 
         }
     })
 })
 
+// sumário do ciclo de pagamentos
 BillingCycle.route('summary', (req, res, next) => {
+    // função aggregate que usa operadores do Mongo
     BillingCycle.aggregate(
-        { $project: {credit: 
-            {$sum: "$credits.value"}, debt: { $sum: "$debts.value" } } 
-        }, 
-        { $group: { _id: null, credit: 
-            { $sum: "$credit"}, debt: {$sum: "$debt"} } 
-        },
+        //$project  tira campos que nao interessa, extrai determinados atributos do objeto
+        { $project: { 
+            credit:  { $sum: "$credits.value"}, 
+            debt: { $sum: "$debts.value" }
+        }}, 
+        //agroupa - GroupBy
+        { $group: { 
+            _id: null, 
+            credit: { $sum: "$credit"}, debt: {$sum: "$debt"} 
+        }},
+        // id = falso, aparecer apenas crédito e débito 1= true
         { $project: { _id: 0, credit: 1, debt: 1 } }, (error, result) => {
             if(error) {
                 res.status(500).json( { errors: [error] } )
             } else {
+            //se caso resultado for nulto, credito 0 e debt 00
             res.json( result[0] || { credit: 0, debt: 0 } )
             }
         }
--- backend/src/api/common/errorHandler.js
+++ backend/src/api/common/errorHandler.js
@@ -1,16 +1,20 @@
 const _ = require('lodash')
+//referencia padrão
 
+//midleware express (request, response, next
+// node restful traz os erros
 module.exports = (req, res, next) => {
     const bundle = res.locals.bundle
 
     if(bundle.errors) {
-        const errors = parseErrors(bundle.errors)
-        res.status(500).json({errors})
+        const errors = parseErrors(bundle.errors) //array com as strings de erros
+        res.status(500).json({errors}) //objeto erros é criado
     } else {
         next()
     }
 }
 
+//Converte para array de mensagens
 const parseErrors = (nodeRestfulErrors) => {
     const errors = []
     _.forIn(nodeRestfulErrors, error => errors.push(error.message))
